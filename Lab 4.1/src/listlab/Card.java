4c1d556338044806051429aebccbbd1155181e60
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-12-13 13:07:42 -0500

    Created Card Class.

---
+++ Lab 4.1/src/listlab/Card.java
@@ -0,0 +1,38 @@
+package listlab;
+
+public class Card {
+
+	private static String rank;
+	private static String suit;
+	private static int pointValue;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	public Card(String rank, String suit, int pointValue)
+	{
+		this.rank=rank;
+		this.suit=suit;
+		this.pointValue=pointValue;
+	}
+	public static String getRank()
+	{
+		return rank;
+	}
+	public static String getSuit()
+	{
+		return suit;
+	}
+	public static int getPointValue()
+	{
+		return pointValue;
+	}
+	public boolean equals(Card otherCard)
+	{
+		return this.getPointValue()==Card.getPointValue();
+	}
+	public String toString()
+	{
+		return this.getPointValue()+"of"+" "+this.getSuit();
+	}
+}
